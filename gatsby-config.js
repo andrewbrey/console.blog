6873a15c8dc3e1357032bcc7fb3d1bbd3831e5b4
Andrew Brey <andrew@fluencyy.com>
2020-03-06 20:44:57 -0800

    refactor(*): remove boilerplate
    get rid of much of the boilerplate provided out of the box by gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
+  siteMetadata: {},
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -27,8 +23,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
